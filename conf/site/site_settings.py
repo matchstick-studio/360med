ca5dba710dc101a65eefb894b3c44e8d2f975471
Collin Rukundo <rukundocollin@gmail.com>
2020-12-10 01:45:14 +0300

    Add a few more app settings imports

--- conf/site/site_settings.py
+++ conf/site/site_settings.py
@@ -4,8 +4,10 @@ import requests
 import logging
 import platform
 
-# from network.settings import *
+from network.settings import *
+from network.emailer.settings import *
 
+from network.account.settings import *
 from network.forum.settings import *
 
 logger = logging.getLogger("network")
