0790b362b32091d36d8b65632726618b3a6c221c
Collin Rukundo <rukundocollin@gmail.com>
2020-12-10 02:04:11 +0300

    Updated settings module on deploy

--- conf/scripts/server-migrate.sh
+++ conf/scripts/server-migrate.sh
@@ -13,7 +13,7 @@ conda activate engine
 set -ue
 
 # Set the configuration module.
-export DJANGO_SETTINGS_MODULE=conf.run.site_settings
+export DJANGO_SETTINGS_MODULE=network.forum.settings
 
 export POSTGRES_HOST=/var/run/postgresql
 
